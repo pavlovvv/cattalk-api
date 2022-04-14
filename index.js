4bf74297a2f033f5d58ae43970e00cc6083acc36
Alex <uapavlof@gmail.com>
2022-04-14 14:08:53 +0300

    corsp4

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Methods", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", "http://localhost:3000")
+    res.header("Access-Control-Allow-Credentials", true)
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
