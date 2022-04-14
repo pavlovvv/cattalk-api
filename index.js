228b23ca2c3f0f7d61769e1c30c4aebd5904e2a8
Alex <uapavlof@gmail.com>
2022-04-14 13:59:36 +0300

    cors+++

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-var cors = require('cors');    
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Methods", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "http://localhost:3000")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   
 
 
