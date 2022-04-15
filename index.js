94106f1a45ffc5ac5a19f83292ee618335c5f6c1
Alex <uapavlof@gmail.com>
2022-04-15 19:39:10 +0300

    cors

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-var cors = require('cors');    
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
   
 
 
