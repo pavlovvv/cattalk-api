7656517b439deaee7150d5e95d14927745689214
Alex <uapavlof@gmail.com>
2022-04-15 20:44:15 +0300

    cors

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 var cors = require('cors');    
-// app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-app.use(cors())
+ app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
 
 
 app.get('/', (req,res) => {
