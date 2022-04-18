a2332aa59b81429d9a48a9f776640f49b8f28fd9
Alex <uapavlof@gmail.com>
2022-04-18 12:40:36 +0300

    logout

--- index.js
+++ index.js
@@ -113,7 +113,6 @@ app.get('/users/checkMyOwnInfo', ValidateCookies, (req, res) => {
 app.delete('/users/logout', (req, res) => {
     if ('CatTalk_userId' in req.cookies) {
         cookies.set('CatTalk_userId', {expires: Date.now()});
-        next()
         return res.status(200).json({msg: 'Success'})
     }
     else {
