4bd1e06695c3e371c27b4d5c5c2e17488c553e16
pavlov <uapavlof@gmail.com>
2022-05-08 01:35:09 +0300

    cors

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ let cors = require('cors');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", 'true');
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Authorization, Origin, Accept, content-type, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  })
+    next()
+})
 
 
 
