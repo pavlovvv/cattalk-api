5b0edc348fe775660d250bc496e3d564acebaf04
pavlov <uapavlof@gmail.com>
2022-05-08 01:10:51 +0300

    toc

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ app.use(cors());
 //     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 //   })
 
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 
 app.get('/', (req, res) => {
     res.send(' can cats talk ?:)')
