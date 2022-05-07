6406b42d590d8af50ea7018a1d54a1194ca4212b
pavlov <uapavlof@gmail.com>
2022-05-08 01:47:52 +0300

    as

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(cors({
     "optionsSuccessStatus": 200
 }));
 
+
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 //     res.header("Access-Control-Allow-Credentials", true);
