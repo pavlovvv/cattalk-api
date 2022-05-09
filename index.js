b391f1dc19b7ae144f11fabfc6e4a07df4c6e6cc
pavlov <uapavlof@gmail.com>
2022-05-10 02:33:44 +0300

    origin

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ let cors = require('cors');
 
 app.use(cors({
     credentials: true,
-    // origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000',
     // origin: 'https://cat-talk2.vercel.app',
     // origin: 'https://cat-talk-l6mh5d0xf-pavlovvv.vercel.app',
-    origin: 'https://cat-talk2-pavlovvv.vercel.app',
+    // origin: 'https://cat-talk2-pavlovvv.vercel.app',
     allowedHeaders: 'Authorization, Origin, X-Requested-With, Access-Control-Request-Headers, content-type, Content-Type, Access-Control-Request-Method, Accept, Access-Control-Allow-Headers',
     "optionsSuccessStatus": 200
 }));
